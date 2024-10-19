6899d437198123885c143061f70809c250807c32
uguremirmustafa <uguremirmustafa@gmail.com>
2024-10-19 13:30:59 +0300

    trigger deploy

--- index.js
+++ index.js
@@ -1,55 +1,18 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const cors = require('cors');
-const https = require('https');
+const http = require('http');
+const httpProxy = require('http-proxy');
 
-const agent = new https.Agent({
-  secureProtocol: 'TLSv1_2_method', // Forcing TLS 1.2
-});
+// Create a new instance of http-proxy
+const proxy = httpProxy.createProxyServer({});
 
-// Use CORS middleware to allow all origins
-app.use(
-  cors({
-    allowedHeaders: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*',
-    },
+// Start the proxy server
+http
+  .createServer((req, res) => {
+    const targetUrl = req.query.url; // Modify the Origin header
+    req.headers['origin'] = targetUrl;
+
+    // Forward the request to the target server
+    proxy.web(req, res, { target: targetUrl });
   })
-);
-
-async function proxyController(req, res) {
-  const targetUrl = req.query.url; // The target URL is passed as a query parameter
-
-  if (!targetUrl) {
-    return res.status(400).send('Please provide a URL.');
-  }
-
-  const axiosObj = {
-    url: targetUrl,
-    method: req.method, // Forward the original HTTP method (GET, POST, etc.)
-    headers: {
-      ...req.headers,
-      origin: targetUrl,
-    },
-    data: req.body, // Forward the request body for POST/PUT
-    httpsAgent: agent, // Use the custom agent with TLS settings
-  };
-  console.log(axiosObj);
-  try {
-    const response = await axios(axiosObj);
-
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).send(`Error: ${error.message}`);
-  }
-}
-
-app.get('/proxy', proxyController);
-app.post('/proxy', proxyController);
-
-// Set Vercel's default port
-app.listen(3000, () => {
-  console.log(process.version);
-  console.log('Proxy server running on port 3000');
-});
+  .listen(3000, () => {
+    console.log('Proxy server is running on port 3000');
+  });
